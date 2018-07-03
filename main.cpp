393642a4ff4cef5c2cc8f4db875660ffec2c9796
boyaozhu <35354916+boyaozhu@users.noreply.github.com>
2018-07-03 12:13:17 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,134 @@
+//
+//  main.cpp
+//  mbpt_pairing
+//
+//  Created by Boyao Zhu on 6/28/18.
+//  Copyright © 2018 Boyao Zhu. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include "armadillo"
+
+using namespace std;
+using namespace arma;
+
+double assym(int, int, int, int, int [8][2], double&);
+double eps(int, int, int, int, int [8][2]);
+double e2nd(double (*assym) (int, int, int, int, int [8][2], double&), double (*eps) (int, int, int, int, int [8][2]), int [4], int [4], int [8][2], double&);
+
+
+int main()
+{
+    int below_fermi[4] = {0,1,2,3};
+    int above_fermi[4] = {4,5,6,7};
+    int mystates[8][2] = {{1,1},{1,-1},{2,1},{2,-1},{3,1},{3,-1},{4,1},{4,-1}};
+    double grid[21];
+    grid[0]=-1;                  //initialize grid space from -1 to 1
+    for (int i=1; i<21; i++)
+    {
+        grid[i]=grid[i-1]+0.1;
+    }
+    
+    double s2;   //second order
+    double g;    //interaction
+    mat Hamiltonian(6,6);
+    vec eigval;
+    double val;
+    for (int i=0; i<21; i++)
+    {
+        g=grid[i];
+        s2=e2nd(&assym, &eps, below_fermi, above_fermi, mystates, g);
+        
+        ///////********************************************/////////////
+        
+        Hamiltonian<<2-g<<-g/2<<-g/2<<-g/2<<-g/2<<0<<endr
+                   <<-g/2<<4-g<<-g/2<<-g/2<<0<<-g/2<<endr
+                   <<-g/2<<-g/2<<6-g<<0<<-g/2<<-g/2<<endr
+                   <<-g/2<<-g/2<<0<<6-g<<-g/2<<-g/2<<endr
+                   <<-g/2<<0<<-g/2<<-g/2<<8-g<<-g/2<<endr
+                   <<0<<-g/2<<-g/2<<-g/2<<-g/2<<10-g<<endr;
+        eig_sym(eigval, Hamiltonian);
+        
+        val=min(eigval)-(2-g);
+        cout<<val<<endl;
+        
+        
+        
+        
+    }
+   
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+double assym(int p, int q, int r, int s, int mystates[8][2], double& pt)
+{ //p,q,r,s start from 0,1,2,3,4,5,6.7
+    
+    int p1=mystates[p][0];
+    int p2=mystates[q][0];
+    int p3=mystates[r][0];
+    int p4=mystates[s][0];
+    int s1=mystates[p][1];
+    int s2=mystates[q][1];
+    int s3=mystates[r][1];
+    int s4=mystates[s][1];
+    if ((p1!=p2) || (p3!=p4))
+        return 0;
+    else if ((s1==s2) || (s3==s4))
+        return 0;
+    else if ((s1==s3) && (s2==s4))
+        return -pt/2;
+    //if ((s1==s4) && (s2==s3))
+    else
+        return pt/2;
+}
+
+
+double eps(int p, int q, int r, int s, int mystates[8][2])
+{
+    double E=0;
+    int p1=mystates[p][0];
+    int p2=mystates[q][0];
+    int p3=mystates[r][0];
+    int p4=mystates[s][0];
+    int ptt[]={p1, p2, p3, p4};
+    //cout<<ptt[0]<<endl<<ptt[1]<<endl<<ptt[2]<<endl<<ptt[3]<<endl;
+    for(int i=0; i<4; i++)
+    {
+        if (ptt[i]<3)
+            E+=((ptt[i])-1);
+        else
+            E-=((ptt[i])-1);
+    }
+    return E;
+}
+
+double e2nd(double (*assym) (int p, int q, int r, int s, int mystates[8][2], double& pt), double (*eps) (int p, int q, int r, int s, int mystates[8][2]), int below_fermi[4], int above_fermi[4], int mystates[8][2], double& g)
+{
+    double s1=0;
+    for (int a=above_fermi[0]; a<=above_fermi[3]; a++)
+    {
+        for (int b=above_fermi[0]; b<=above_fermi[3]; b++)
+        {
+            for (int i=below_fermi[0]; i<=below_fermi[3]; i++)
+            {
+                for (int j=below_fermi[0]; j<=below_fermi[3]; j++)
+                {
+                    s1+=0.25*assym(a,b,i,j,mystates,g)*assym(i,j,a,b,mystates,g)/eps(a,b,i,j,mystates);
+                    //cout<<s1<<endl;
+                }
+            }
+        }
+    }
+    return s1;
+}
+
+
